7ed650b76bc511e4f3408c4f6d2112888eab7ce7
SoSad803 <gavrilov_nikita08@mail.ru>
2025-11-12 10:51:21 +0300

    Add files via upload

---
+++ all (1).cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+
+int main() 
+{
+    int x, y, max;
+    cin >> x >> y;
+    (x > y) ? cout << x : cout << y << endl; // 1
+    max = (x > y) ? x : y; //2
+    cout << max << endl;
+    return 0;
+
+}
